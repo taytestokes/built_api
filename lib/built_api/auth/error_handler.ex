825f79f631ef3ce761ec660b994440f647755c4a
taytestokes <taytestokes9328@gmail.com>
2022-07-03 13:58:11 -0600

    Builds the custom pipeline and error handler plug for guardian authentication

---
+++ lib/built_api/auth/error_handler.ex
@@ -0,0 +1,19 @@
+# Custom plug to handle errors for Guardian
+# when trying to authenticate access tokens
+defmodule BuiltApi.Auth.ErrorHandler do
+    import Plug.Conn
+
+    @behaviour Guardian.Plug.ErrorHandler
+
+    @impl Guardian.Plug.ErrorHandler
+
+    def auth_error(conn, {type, _reason}, _opts) do
+        body = Jason.encode!(${error: to_string(type)})
+
+        # Since this is a JSON based API, we will return
+        # JSON data for the error instead of plain text
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(401, body)
+    end
+end
--- lib/built_api/auth/pipeline.ex
+++ lib/built_api/auth/pipeline.ex
@@ -1,1 +1,14 @@
-# Custom pipeline that will be used for HTTP requests
+# Custom pipeline that will be used in the router for HTTP requests so the guardian implementation module can watch for session or header the jwt tokens used for authentication
+defmodule BuiltApi.Auth.Pipeline do
+    use Guardian.Plug.Pipeline,
+        otp_app: :built_api,
+        module: BuiltApi.Auth.Guardian,
+        error_handler: BuiltApi.Auth.ErrorHandler
+    
+    # If there is a session token, restrict it to an access token and validate it
+    plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
+    # If there is a header token, restrict it to an access token and validate it
+    plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}
+    # Load the user if either verifcation has worked
+    plug Guardian.Plug.LoadResource, allow_blank: true
+end
